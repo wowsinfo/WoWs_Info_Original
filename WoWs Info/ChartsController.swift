3215d75cc2c5528e43ecd1bf41f774f013cf446e
H.Q <henryquan@H-E-N-R-Y.local>
2017-02-19 23:30:16 +1100

    UI changes
    Cells are redesigned
    Search button is removed
    
    
    Former-commit-id: 4d2a90d1229d711904789fe13daf0383dabcae14
    Former-commit-id: 995d72a099a120eda8dbf5c20477c84375eca767
    Former-commit-id: f088ebb1acf624f1c191395d7035f6da95e37b7e

---
+++ WoWs Info/ChartsController.swift
@@ -0,0 +1,23 @@
+//
+//  ChartsController.swift
+//  WoWs Info
+//
+//  Created by Henry Quan on 19/2/17.
+//  Copyright © 2017 Henry Quan. All rights reserved.
+//
+
+import UIKit
+
+class ChartsController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
