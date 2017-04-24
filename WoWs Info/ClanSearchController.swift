0ef440c1179b018d0b179834a6125e3d489986de
H.Q <henryquan@H-E-N-R-Y.local>
2017-04-24 22:08:22 +1000

    Clan API
    Clan class is added to get Clan ID and Clan information.
    
    
    Former-commit-id: 5c81f5c4eb4136e5888542f773968cbab03703e6
    Former-commit-id: 93b0425c4c90fe53d6473ca722b3a4ea0731e150
    Former-commit-id: 614588d9204c75920e883285399e7dfa659677b1

---
+++ WoWs Info/ClanSearchController.swift
@@ -0,0 +1,35 @@
+//
+//  ClanSearchController.swift
+//  WoWs Info
+//
+//  Created by Henry Quan on 24/4/17.
+//  Copyright © 2017 Henry Quan. All rights reserved.
+//
+
+import UIKit
+
+class ClanSearchController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
